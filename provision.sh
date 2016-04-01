400e11ecdbfb6f4a5a7f434e22d7eae4e1e535d9
sickmoses <sickmoses@gmail.com>
2016-03-31 17:54:22 -0700

    Success, we installed mongo db onto ubuntu server

--- provision.sh
+++ provision.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 apt-get update
